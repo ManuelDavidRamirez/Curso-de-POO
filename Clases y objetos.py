02aaf65b5d62857885e2c78a3290ed1d840802fb
Manuel David Ramirez <Manuyagami95@gmail.com>
2023-11-06 15:00:06 -0300

    Métodos

--- Clases y objetos.py
+++ Clases y objetos.py
@@ -1,10 +1,16 @@
-class Celular():
+class Celular:
     def __init__(self, marca, modelo, camara):
         self.marca = marca
         self.modelo = modelo
         self.camara = camara
 
+    def llamar(self):
+        print(f"Estas haciendo un llamado desde un: {self.modelo}")
+
+    def cortar(self):
+        print(f"Cortaste la llamada desde tu: {self.modelo}")
+
 celular1 = Celular("Samsung","S23","48MP")
 celular2 = Celular("Apple","Iphone 15 pro","96MP")
 
-print(celular2.marca)
+celular2.llamar()
